ab0df5c42fbfe1a337f44140beb621b72fefb795
Codando Simples <codandosimples@gmail.com>
2022-04-06 11:25:59 -0300

    Creates helper class to copy only not null properties

---
+++ src/main/java/com/codandosimples/helper/NullAwareBeanUtilsBean.java
@@ -0,0 +1,17 @@
+package com.codandosimples.helper;
+
+import org.apache.commons.beanutils.BeanUtilsBean;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.InvocationTargetException;
+
+@Component
+public class NullAwareBeanUtilsBean extends BeanUtilsBean {
+
+	@Override
+	public void copyProperty(Object dest, String name, Object value) throws IllegalAccessException, InvocationTargetException {
+		if (value == null)
+			return;
+		super.copyProperty(dest, name, value);
+	}
+}
